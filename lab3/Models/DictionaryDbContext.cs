cf4c23c375c45655a57808b606e06091d40fcbc2
OvcharenkoDaniil <danila-bras@mail.ru>
2022-10-25 17:54:50 +0300

    finally...

---
+++ lab3/Models/DictionaryDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+namespace lab3.Models
+{
+    
+        public class DictionaryDbContext : System.Data.Entity.DbContext
+        {
+            public DbSet<DictionaryItem> DictionaryItems { get; set; }
+
+            public DictionaryDbContext() : base("DefaultConnection")
+            {
+
+            }
+        }
+    
+}
